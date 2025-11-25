945cac91dc2d7f73e5500708c859c27e90492169
HichamAl <104017860+HichamAl@users.noreply.github.com>
2025-11-25 14:47:18 +0100

    added eventlistener onmouseover which shows the dropdown menu items

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("test");
+const items = document.querySelectorAll(".items");
+const menu = document.querySelector(".toggle");
+
+menu.addEventListener("mouseover", function (e) {
+    items.forEach((item)=> {
+        item.removeAttribute("hidden");
+    })
+})
+
